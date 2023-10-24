e748b7b3cd56ac8c5421302d0155fafcfce7bc77
rufatsoltanli <146984697+rufatsoltanli@users.noreply.github.com>
2023-10-25 02:00:01 +0300

    Add files via upload

---
+++ assets/js/script.js
@@ -0,0 +1,140 @@
+// 1-ci task
+
+// console.log("task-1");
+
+// let num = +prompt("task-1. reqemi daxil et")
+// for (let i = 2; i <= num; i *= 2) {
+
+//     if (i == num) {
+//         console.log("beli");
+
+//     }
+
+// }
+
+
+// 2-ci task
+
+// console.log("task-2");
+
+// let num1 = +prompt("task-2. reqem daxil et")
+// let num2 = +prompt("reqemin quvvesin daxil et")
+// let num3
+//     num3 = (num1 ** num2)
+//     console.log(num3);
+
+
+
+// 3-ci task   
+// console.log("task-3");
+// let sum1 = 0
+// let sum2 = 0
+// for (let i = 11; i < 100; i += 11) {
+//     sum1 += i
+
+
+// }
+// for (let i = 100; i < 1000; i++) {
+//     if (i % 111 == 0) {
+//         sum2 += i
+//     }
+
+// }
+// console.log(sum1+sum2);
+
+// 4-ci task
+// console.log("task-4");
+// const array=[11,42,52,34,44,56,59,78]
+// let max = array[0]
+// for (let i = 0; i < array[i]; i++) {
+//     if (array[i] % 2 ==0 && array[i]>max) {
+//         max=array[i]
+
+//     }
+
+// }
+// console.log(max);
+
+
+// 5-ci task
+
+// console.log("task-5");
+// const array = [11, 42, 52, 5, 34, 44, 56, 59, 78]
+// let min = array[0]
+// for (let i = array.length; i > 0; i--) {
+//     if (array[i] % 2 == 1 && min> array[i]) {
+//         min = array[i]
+//         if (min % array [i] == 0) {
+//             console.log(i);
+//         }
+//     }
+
+// }
+
+
+//6-ci task yaza bilmedim
+
+// const array = [3, 6, 9, 11, 13, 15, 17]
+// let num = array[0]
+// let count = 0
+
+// for (let i = 2; i < num; i++) {
+//     if (num % i == 0) {
+//         count++
+//         console.log(count + " murekkeb");
+//     }
+
+// }
+
+// 7-ci task
+// console.log("task-7");
+// const array = [11, 42, 52, 5, 34, 44, 56, 59, 78]
+// let min = array[0]
+// let max = array[0]
+// let minnum = 0
+// let maxnum = 0
+// let sum = 0
+// let ortalama = 0
+// let minmaxsum=0
+// for (let i = array.length; i > 0; i--) {
+//     if (min > array[i]) {
+//         min = array[i]
+//         minnum = min
+
+//     }
+// }
+// for (let i = 0; i < array.length; i++) {
+//     if (max < array[i]) {
+//         max = array[i]
+//         maxnum = max
+//     }
+
+
+// }
+// for (let i = 0; i < array.length; i++) {
+
+//     sum+=array[i]
+   
+    
+// }
+// ortalama = sum/array.length
+// minmaxsum = minnum + maxnum
+// if (minmaxsum>ortalama) {
+//     console.log("beli coxdur.");
+    
+// }
+
+
+// task8
+
+// let number=prompt("enter number")
+// let s =("" + number).split("")
+// let highest=s[0]
+// for (let i = 0; i < s[i]; i++) {
+//     if (highest<s[i]) {
+//         highest=s[i]
+
+//     }
+    
+// }
+// console.log(highest);
